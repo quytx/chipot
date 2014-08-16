c80e87cfc0bfc0cafaf66c59b4a848fdb917a2ff
Ismail <ismailkalimi@gmail.com>
2014-08-16 17:57:08 -0500

    Seeding done for 5 days

--- Gemfile
+++ Gemfile
@@ -32,6 +32,10 @@ gem "mini_magick"
 gem 'jquery-ui-rails'
 gem 'angularjs-rails'
 
+
+#City of Chicago API
+gem 'soda-ruby'
+
 # photo
 gem 'carrierwave'
 gem 'fog'
@@ -47,4 +51,3 @@ gem 'fog'
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'soda/client'
+
+client = SODA::Client.new({:domain => "data.cityofchicago.org", :app_token => ENV["App_Token"]})
+count = 0
+6.times do
+  response = client.get("7as2-ds3y",{creation_date: "2014-08-1#{count}T00:00:00"})
+  response.each do |element|
+    element = element.to_hash
+    values = {
+      creation_date: element["creation_date"],
+      completion_date: element["completion_date"],
+      status: element["status"],
+      street_address: element["street_address"],
+      zip: element["zip"],
+      latitude: element["latitude"],
+      longitude: element["longitude"],
+      service_request_number: element["service_request_number"],
+      current_activity: element["current_activity"],
+      most_recent_action: element["most_recent_action"]
+    }
+
+    Pothole.create(values)
+  end
+  count +=1
+end
