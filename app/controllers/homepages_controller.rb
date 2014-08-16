d007ce7e36c1b92144c1254f35cfec4a07ffc20c
quytx1102 <quytx1102@gmail.com>
2014-08-16 18:41:14 -0500

    markers loaded

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,12 @@
 class HomepagesController < ApplicationController
   def index
   end
+
+  def getPotholes
+    @holes = Pothole.all
+    respond_to do |format|
+      format.html 
+      format.json { render :json => @holes }
+    end
+  end
 end
