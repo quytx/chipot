7839a7baf35eaea7ea705077ee0aadd20ae787e3
Ismail <ismailkalimi@gmail.com>
2014-08-16 20:54:14 -0500

    more javascript

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -10,7 +10,9 @@ $(function() {
 
     var map = initialize(mapOptions);
     var infowindow = new google.maps.InfoWindow();
-    var markers = [];
+    var marker = "";
+    var unfilled_markers = [];
+    var filled_markers = [];
 
     function makeInfoWindowEvent(map, infowindow, contentString, marker) {
         google.maps.event.addListener(marker, 'click', function() {
@@ -24,31 +26,50 @@ $(function() {
         url: "/potholes.json",
         success: function(data) {
             for (var i = 0; i < data.length; i++) {
-                if (data[i].completion_date == null) {
-                    var marker = new google.maps.Marker({
+                if (data[i].completion_date === null) {
+                    marker = new google.maps.Marker({
                         position: new google.maps.LatLng(data[i].latitude, data[i].longitude),
                         map: map,
                         icon: '/assets/red_MarkerA.png'
                     });
+                    makeInfoWindowEvent(map, infowindow, "Reported on: " + data[i].creation_date + "<br>" + "Street Address: " + data[i].street_address, marker);
+                    unfilled_markers.push(marker);
                 } else {
-                    var marker = new google.maps.Marker({
+                    marker = new google.maps.Marker({
                         position: new google.maps.LatLng(data[i].latitude, data[i].longitude),
                         map: map,
                         icon: '/assets/green_MarkerA.png'
                     });
-
+                    makeInfoWindowEvent(map, infowindow, "Reported on: " + data[i].creation_date + "<br>" + "Completed Date: " + data[i].completion_date + "<br>" + "Street Address: " + data[i].street_address, marker);
+                    filled_markers.push(marker);
                 }
-
-                makeInfoWindowEvent(map, infowindow, "test" + i, marker);
-
-                markers.push(marker);
             }
         },
         dataType: "json"
     });
 
+    $(document).ajaxSuccess(function() {});
+
     google.maps.event.addListener(map, 'click', function(event) {
         infowindow.close();
     });
+
+
+
+    $("#filled").on('click', function() {
+        if ($("#filled").prop("checked")) {
+            console.log("this is checked");
+            for (var i = 0; i < filled_markers.length; ++i) {
+                filled_markers[i].setVisible(false);
+            }
+        } else {
+            console.log("this is unchecked");
+            for (var i = 0; i < filled_markers.length; ++i) {
+                filled_markers[i].setVisible(true);
+            }
+        }
+
+    });
+
 
 });
