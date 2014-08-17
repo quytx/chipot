e218654e1313ab49a2a21ed26d9afb9411c30628
Ismail <ismailkalimi@gmail.com>
2014-08-17 12:10:34 -0500

    working on submit form

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -5,8 +5,20 @@ class HomepagesController < ApplicationController
   def getPotholes
     @holes = Pothole.all
     respond_to do |format|
-      format.html 
+      format.html
       format.json { render :json => @holes }
     end
   end
+
+
+  def renderReport
+    @report = Pothole.new
+    render partial: "reportForm"
+  end
+
+  def submitReport
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/potholes' => 'homepages#getPotholes'
 
+  get '/report' => 'homepages#renderReport'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
