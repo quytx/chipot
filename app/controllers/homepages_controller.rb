138864fa6002a50292441b020bf6647cb75ba594
Ismail <ismailkalimi@gmail.com>
2014-08-17 12:14:05 -0500

    working on submit form

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -13,6 +13,24 @@ class HomepagesController < ApplicationController
 
   def renderReport
     @report = Pothole.new
+
+    uri = URI.parse('http://test311request.cityofchicago.org/open311/v2/requests.json')
+    API_KEY = ENV["open311_key"]
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data({'api_key' => API_KEY,
+                           'service_code'=> '4fd3b656e750846c53000004',
+                           'lat' => 41.881232,
+                           'long'=> -87.632719,
+                           'address_string' => "Test 2",
+                           'description' => "Testing",
+                           'attribute[WHEREIST]'=> "CURB"})
+
+    response = http.request(request)
+    msg = response.body
+
+    token = JSON.parse(msg).pop["token"]
     render partial: "reportForm"
   end
 
