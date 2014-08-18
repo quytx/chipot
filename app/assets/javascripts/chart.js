65ed6ff85b23078519bfbdd773ee40ffa8b8c232
Lucas Willett <me@ltw.io>
2014-08-18 15:34:05 -0500

    Cleanup javascript formatting.

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,25 +1,57 @@
 var app = angular.module('chartApp', ['PotHoleChartController'])
 
-app.controller('PotHoleChartController', ['$scope', function($scope){
-    $scope.data=[];
+app.controller('PotHoleChartController', ['$scope',
+  function($scope) {
+    $scope.data = [];
 
     $scope.dates = [];
 
     $scope.options = {
       axes: {
-        x: {key: 'x', labelFunction: function(value) {return $scope.dates[value];}, type: 'linear'},
-        y: {type: 'linear', min: 0, max: 400},
-        y2: {type: 'linear', min: 0, max: 400}
+        x: {
+          key: 'x',
+          labelFunction: function(value) {
+            return $scope.dates[value];
+          },
+          type: 'linear'
+        },
+        y: {
+          type: 'linear',
+          min: 0,
+          max: 400
+        },
+        y2: {
+          type: 'linear',
+          min: 0,
+          max: 400
+        }
       },
-      series: [
-        {y: 'report', color: 'red', thickness: '2px', label: 'Reported'},
-        {y: 'patch', axis: 'y2', color: 'green', type: 'area', striped: true, visible: false, drawDots: true, label: 'Patched'}
-      ],
+      series: [{
+        y: 'report',
+        color: 'red',
+        thickness: '2px',
+        label: 'Reported'
+      }, {
+        y: 'patch',
+        axis: 'y2',
+        color: 'green',
+        type: 'area',
+        striped: true,
+        visible: false,
+        drawDots: true,
+        label: 'Patched'
+      }],
       lineMode: 'linear',
       tension: 0.7,
-      tooltip: {mode: 'scrubber', formatter: function(x, y, series) {return y;}},
+      tooltip: {
+        mode: 'scrubber',
+        formatter: function(x, y, series) {
+          return y;
+        }
+      },
       drawLegend: true,
       drawDots: true,
       columnsHGap: 5
     }
-}]);
+  }
+]);
