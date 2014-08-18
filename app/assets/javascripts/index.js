eb0d64958ce05c0e6af6efac58a982a87ae479cd
Max Diener <mvdiener@gmail.com>
2014-08-18 13:43:35 -0500

    Added field for phone number on report form

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,7 +9,7 @@ $(function() {
     };
 
     var clusterOptions = {
-        gridSize: 50,
+        gridSize: 70,
         maxZoom: 14
     };
 
@@ -90,6 +90,8 @@ $(function() {
                                       <option value='INTERSEC'>Intersection</option>\
                                       <option value='TRAFFIC'>Traffic Lane</option>\
                                     </select>\
+                                    <label for='activity'>Enter phone # to receive text updates about your request (optional):</label>\
+                                    <input type='text' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' name='activity' placeholder='###-###-####'>\
                                     <br>\
                                     <br><input type='submit' value='Submit'>\
                                 </form>"
