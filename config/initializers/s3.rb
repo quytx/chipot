1870f7a7ea451fb9013e3492d01768069c755526
quytx1102 <quytx1102@gmail.com>
2014-08-20 09:08:22 -0500

    upload photo function added

---
+++ config/initializers/s3.rb
@@ -0,0 +1,12 @@
+require "carrierwave"
+require "carrierwave/orm/activerecord"
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => 'us-west-2'
+  }
+  config.fog_directory  = 'chicaghole'
+end
