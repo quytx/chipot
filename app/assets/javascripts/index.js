09f22f67a4c8f0e66df94244a276709755507e98
Max Diener <mvdiener@gmail.com>
2014-08-20 14:44:57 -0500

    Added geolocator

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -202,6 +202,51 @@ $(function() {
     }, 'json');
   });
 
+  // This is getting the location
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+        position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'You are Here!'
+      });
+      setTimeout(function() {
+        infowindow.close();
+      }, 2000);
+      map.setZoom(18);
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+      var content = 'Error: The Geolocation service failed.';
+    } else {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+      map: map,
+      position: new google.maps.LatLng(60, 105),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
+  }
+
+  //geting location is ending here
+
   getDatesBetween = function(startDate, endDate) {
     var start = new Date(startDate);
     var end = new Date(endDate);
